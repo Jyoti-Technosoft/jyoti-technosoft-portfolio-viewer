142719b6d5a4e7689fc16c75cec803d84d979015
Dipesh Gandhi <dipesh_gandhi9@yahoo.com>
2023-03-06 13:11:43 +0530

    Jyoti technosoft Portfolio : Add z-index to fix div overlayering issue

--- src/components/FooterComponent/FooterLayout.js
+++ src/components/FooterComponent/FooterLayout.js
@@ -3,30 +3,31 @@ import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { positions } from '@mui/system';
+import Link from '@mui/material/Link';
+import "./footerStyle.scss"
 
 const FooterLayout = () => {
   return (
     <div className="footer-wrapper">
-        <div className="container center">
-        <a href="https://www.facebook.com/info.jyotitechnosoft/?ref=py_c" className="btn-floating waves-effect waves-light grey darken-3">
+        <div className="container justify-content-center">
+        <Link href="https://www.facebook.com/info.jyotitechnosoft/?ref=py_c" className="btn-floating waves-effect waves-light grey darken-3">
             <FacebookRoundedIcon fontSize="large"/>
-            </a>
-            <a href="https://g.page/jyoti-technosoft?gm" className="btn-floating waves-effect waves-light grey darken-3">
+            </Link>
+            <Link href="https://g.page/jyoti-technosoft?gm" className="btn-floating waves-effect waves-light grey darken-3">
                 <i className="fa fa-google-plus-square"></i>
-            </a>
-            <a href="https://twitter.com/JyotiTechnosoft" className="btn-floating waves-effect waves-light grey darken-3">
+            </Link>
+            <Link href="https://twitter.com/JyotiTechnosoft" className="btn-floating waves-effect waves-light grey darken-3">
                 <TwitterIcon fontSize="large"/>
-            </a>
-            <a href="https://in.linkedin.com/company/jyoti-technosoft" className="btn-floating waves-effect waves-light grey darken-3">
+            </Link>
+            <Link href="https://in.linkedin.com/company/jyoti-technosoft" className="btn-floating waves-effect waves-light grey darken-3">
                 <LinkedInIcon fontSize="large"/>
-            </a>
-            <a href="https://github.com" className="btn-floating waves-effect waves-light grey darken-3">
+            </Link>
+            <Link href="https://github.com" className="btn-floating waves-effect waves-light grey darken-3">
                 <GitHubIcon fontSize="large"/>
-            </a>
+            </Link>
         </div>
         <div className="footer-copyright grey lighten-3 grey-text text-darken-3">
-        <div className="container center">© 2023 Jyoti Technosoft LLP</div>
+        <div className="container">© 2023 Jyoti Technosoft LLP</div>
         </div>
     </div>
   )
